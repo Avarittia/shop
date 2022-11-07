3b36ddee27f8a14eacb02e6b71366e76013dbf95
Nilesh Shrestha <nileshshr97@gmail.com>
2022-11-07 14:54:49 +0545

    final commit

--- src/Components/NewOffers.jsx
+++ src/Components/NewOffers.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import { addToCart } from "./AddToCart";
 
 const NewOffers = () => {
@@ -26,67 +27,70 @@ const NewOffers = () => {
   }, []);
   return (
     <Container fluid className="pt-5">
-      <h3 className="text-center" style={{fontSize:"3rem"}}>New Offers</h3>
+      <h3 className="text-center" style={{ fontSize: "3rem" }}>
+        New Offers
+      </h3>
       <Container className="py-0 px-5">
-      <Row>
-        {items.map((item) => {
-          return (
-            <Col lg={4} md={6} sm={12} className="g-5 px-5">
-              <Card
-                style={{
-                  height: "350px",
-                  gap: "2rem",
-                  borderRadius: "0",
-                }}
-                className="d-flex flex-column justify-content-center align-items-center py-3 card"
-              >
-                <Link to={`${item.id}`}>
-                  {" "}
-                  <Card.Img
-                    variant="top"
-                    src={item.icon}
-                    height="150px"
-                    width="150px"
-                    style={{ objectFit: "cover", width: "150px" }}
-                  />
-                </Link>
+        <Row>
+          {items.map((item) => {
+            return (
+              <Col lg={4} md={6} sm={12} className="g-5 px-5">
+                <Card
+                  style={{
+                    height: "350px",
+                    gap: "2rem",
+                    borderRadius: "0",
+                  }}
+                  className="d-flex flex-column justify-content-center align-items-center py-3 card"
+                >
+                  <Link to={`${item.id}`}>
+                    {" "}
+                    <Card.Img
+                      variant="top"
+                      src={item.icon}
+                      height="150px"
+                      width="150px"
+                      style={{ objectFit: "cover", width: "150px" }}
+                    />
+                  </Link>
 
-                <Card.Body style={{ width: "100%" }}>
-                  <Card.Title
-                    className="text-center"
-                    style={{ fontSize: ".9rem", fontWeight: "500" }}
-                  >
-                    {item.title}
-                  </Card.Title>
-                  <Card.Title
-                    className="text-center mb-2"
-                    style={{ fontSize: "1.1rem", fontWeight: "500" }}
-                  >
-                    <span>$</span>
-                    <span>250</span>
-                  </Card.Title>
-                  <div className="mt-auto">
-                    <Button
-                      className="w-100 add-cart-button"
-                      style={{
-                        backgroundColor: "#3399cc",
-                        border: "none",
-                        borderRadius: "0",
-                      }}
-                      onClick={(e) => {
-                        addToCart(e, item.id, item.id);
-                      }}
+                  <Card.Body style={{ width: "100%" }}>
+                    <Card.Title
+                      className="text-center"
+                      style={{ fontSize: ".9rem", fontWeight: "500" }}
                     >
-                      Add to Cart
-                    </Button>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          );
-        })}
-      </Row>
-    </Container>
+                      {item.title}
+                    </Card.Title>
+                    <Card.Title
+                      className="text-center mb-2"
+                      style={{ fontSize: "1.1rem", fontWeight: "500" }}
+                    >
+                      <span>$</span>
+                      <span>250</span>
+                    </Card.Title>
+                    <div className="mt-auto">
+                      <Button
+                        className="w-100 add-cart-button"
+                        style={{
+                          backgroundColor: "#3399cc",
+                          border: "none",
+                          borderRadius: "0",
+                        }}
+                        onClick={(e) => {
+                          addToCart(e, item.id, item.id);
+                        }}
+                      >
+                        Add to Cart
+                      </Button>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+      <ToastContainer/>
     </Container>
   );
 };
